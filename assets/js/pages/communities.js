a008bb0b2d1f472723935f0f2edf4ab831228624
Gulherme Lemos <guilherls@proton.me>
2026-02-21 21:32:41 -0300

    v5.9

--- assets/js/pages/communities.js
+++ assets/js/pages/communities.js
@@ -1,17 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
 	const communitiesSection = document.getElementById("communities");
+	let flags = {};
 
-	fetch("/communities/communities.json")
+	fetch("/communities/flags.json")
 		.then((response) => response.json())
-		.then((communities) => {
-			communities.forEach((community) => {
-				const article = document.createElement("article");
-				article.className = "community";
-				article.id = community.id;
+		.then((emojis) => {
+			flags = emojis;
+		})
+		.then(() => {
+			fetch("/communities/communities.json")
+				.then((response) => response.json())
+				.then((communities) => {
+					communities.forEach((community) => {
+						const article = document.createElement("article");
+						article.className = "community";
+						article.id = community.id;
 
-				article.innerHTML = `
+						article.innerHTML = `
 				<span class="id" aria-hidden="true" onclick="copyID('${community.id}')" title="Copy ID">#${community.id}</span>
-				${community.lang ? `<div class="language"><span class="flag">${community.flag}</span>${community.lang}</div>` : ""}
+				${community.lang ? `<div class="language"><span class="flag">${flags[community.lang] || "??"}</span>${community.lang}</div>` : ""}
 				${community.rep ? `<span class="rep">Rep: ${community.rep}</span>` : ""}
 				${community.platform ? `<div class="platform"><svg aria-hidden="true" class="platform-icon ${community.platform.toLowerCase()}"><use href="#${community.platform.toLowerCase()}"/></svg> ${community.platform.charAt(0).toUpperCase() + community.platform.slice(1)}</div>` : ""}
 				${community.platforms ? `<div class="platforms">${community.platforms.map((platform) => `<div class="platform"><svg aria-hidden="true" class="platform-icon ${platform.toLowerCase()}"><use href="#${platform.toLowerCase()}"/></svg> ${platform.charAt(0).toUpperCase() + platform.slice(1)}</div>`).join("")}</div>` : ""}
@@ -24,33 +31,39 @@ document.addEventListener("DOMContentLoaded", () => {
 				<p>${community.description}</p>
 				${
 					community.urls
-						? `<div class="community-urls">${Object.entries(
+						? `<strong class="join">Join the Community</strong> <div class="community-urls">${Object.entries(
 								community.urls,
 							)
 								.map(
 									([platform, url]) =>
-										`<a class="join-button" href="${url}" rel="noopener noreferrer" target="_blank"><svg aria-hidden="true" class="platform-icon ${platform.toLowerCase()}"><use href="#${platform.toLowerCase()}"/></svg> ${platform.charAt(0).toUpperCase() + platform.slice(1)}</a>`,
+										`<a class="join-button" href="${url}" rel="noopener noreferrer" target="_blank"><svg aria-hidden="true" class="platform-icon ${platform.toLowerCase()}"><use href="#${platform.toLowerCase()}"/></svg>${platform.charAt(0).toUpperCase() + platform.slice(1)}</a>`,
 								)
 								.join("")}</div>`
 						: `<a href="${community.url}" class="join-button" rel="external noopener noreferrer" target="_blank">Join the Community</a>`
 				}
 			`;
 
-				communitiesSection.appendChild(article);
-				article.addEventListener("click", () => {
-					article.scrollIntoView({ behavior: "smooth" });
-				});
-			});
-
-			Array.from(communitiesSection.children).forEach((community) => {
-				if (window.location.hash === `#${community.id}`) {
-					requestAnimationFrame(() => {
-						community.classList.add("target");
+						communitiesSection.appendChild(article);
+						article.addEventListener("click", () => {
+							article.scrollIntoView({ behavior: "smooth" });
+						});
 					});
-				}
-			});
+
+					Array.from(communitiesSection.children).forEach(
+						(community) => {
+							if (window.location.hash === `#${community.id}`) {
+								requestAnimationFrame(() => {
+									community.classList.add("target");
+								});
+							}
+						},
+					);
+				})
+				.catch((error) =>
+					console.error("Error loading communities:", error),
+				);
 		})
-		.catch((error) => console.error("Error loading communities:", error));
+		.catch((error) => console.error("Error loading flags:", error));
 });
 
 function copyID(id) {
